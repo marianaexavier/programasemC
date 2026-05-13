17d4f2c3453cc93d5c077631b988769cae245e50
Mariana Xavier <mariannaexavier@gmail.com>
2026-05-13 09:01:29 -0300

    exercicio 6

--- lista1/p6.c
+++ lista1/p6.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-int main () [
-
+int main () {
+    
     float x, y;
 
     printf("Informe duas coordenadas x e y separadas por espaco: ");
     scanf("%f %f", &x, &y);
 
-    
+    if (x>0 && y>0) {
+        printf("Quadrante 1");
+    } else if (x>0 && y<0) {
+        printf("Quadrante 2");
+    } else if (x!=0 && y == 0) {
+        printf("Eixo x");
+    } else if (x == 0 && y!=0) {
+        printf("Eixo y");
+    } else if (x == 0  && y == 0) {
+        printf("Origem");
+    } else if (x<0 && y<0) {
+        printf("Quadrante 3");
+    } else {
+        printf("Quadrante 4");
+    }
 
     return 0;
-]
+}
